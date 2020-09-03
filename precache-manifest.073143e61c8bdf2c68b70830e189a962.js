fc4130a178dac378a8e9a244bf3dc01b878aefa4
Paweł Walczewski <paulwalczewski@icloud.com>
2020-09-03 22:12:20 +0100

    Updates

---
+++ precache-manifest.073143e61c8bdf2c68b70830e189a962.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4807fcc0b7206429705d0fff55fe54e",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "cf3f96388e8d135df332",
+    "url": "/react-weather-app/static/css/2.14fbd4c8.chunk.css"
+  },
+  {
+    "revision": "0e81bf31ebb81f2e7737",
+    "url": "/react-weather-app/static/css/main.d8ac1276.chunk.css"
+  },
+  {
+    "revision": "cf3f96388e8d135df332",
+    "url": "/react-weather-app/static/js/2.5eb65559.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-weather-app/static/js/2.5eb65559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e81bf31ebb81f2e7737",
+    "url": "/react-weather-app/static/js/main.8e4cb77c.chunk.js"
+  },
+  {
+    "revision": "8939bd8769851b270234",
+    "url": "/react-weather-app/static/js/runtime-main.34187cdb.js"
+  }
+]);
